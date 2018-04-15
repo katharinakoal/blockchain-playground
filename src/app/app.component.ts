c546c9bca7e92360026699f915bc3c1c42af968d
kkoal <k.koal@student.htw-berlin.de>
2018-04-15 18:47:17 +0200

    Merkletree and crypto experiments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SHA256 } from 'crypto-js';
+import MerkleTools from '@settlemint/merkle-tools';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'blockchain playground';
+
+  mroot: string;
+
+  constructor() { }
+
+  ngOnInit() {
+
+    console.log('hash x', SHA256('x').toString());
+    console.log('hash y', SHA256('y').toString());
+    console.log('hash z', SHA256('z').toString());
+
+    const merkleTools = new MerkleTools(); // no options, defaults to sha-256 hash type
+
+    merkleTools.addLeaves(['x', 'y', 'z'], true); // we must indicate these values need to be hashed
+
+    merkleTools.makeTree();
+
+    const merkleRoot = merkleTools.getMerkleRoot();
+
+    const proof0 = merkleTools.getProof(0);
+    const proof1 = merkleTools.getProof(1);
+    const proof2 = merkleTools.getProof(2);
+
+    console.log(proof0);
+
+    merkleTools.resetTree(); // use this when done with this tree and you intend on creating a new one
+
+  }
+
 }
